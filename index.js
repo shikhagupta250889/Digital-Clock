049b57ba7547045cae6c00a0522f7537c3f55b3c
Shikha Gupta <shikhagupta.250889@gmail.com>
2018-12-26 01:37:54 +0100

    Completed digital time functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var twelveHourtimeFormat = false;
 function printTime() {
   document.getElementById('digitaltime').innerHTML = requiredTimeFormat();
 }
@@ -15,7 +16,9 @@ function requiredTimeFormat() {
   var Hr = d.getHours();
   var Min = d.getMinutes();
   var Sec = d.getSeconds();
-  var nFormat = `${doubleDigit(Hr)}:${doubleDigit(Min)}:${doubleDigit(Sec)}`;
+  var timeSuffix = addAmPm(Hr);
+  if (twelveHourtimeFormat) Hr = changeTo12Hr(Hr);
+  var nFormat = `${doubleDigit(Hr)}:${doubleDigit(Min)}:${doubleDigit(Sec)} ${timeSuffix}`;
   return nFormat;
 }
 
@@ -23,3 +26,29 @@ function doubleDigit(x) {
   if (x <= 9) return `0${x}`;
   return x;
 }
+
+function changeTo12Hr(x) {
+  if (x > 12 && x < 24)
+    return (x - 12);
+  return x;
+}
+
+function changeTimeFormat() {
+  twelveHourtimeFormat = !twelveHourtimeFormat;
+  changeButtonText();
+}
+
+function changeButtonText() {
+ var buttonText = "Change to 12-Hour Mode";
+  if (twelveHourtimeFormat) buttonText = "Change to 24-Hour Mode";
+  document.querySelector('.toggleButton').innerHTML = buttonText;
+}
+
+function addAmPm(x) {
+  if (twelveHourtimeFormat) {
+    if (x >= 12) return `PM`;
+    return `AM`;
+  }
+    return '';
+}
+// Date Functionality
